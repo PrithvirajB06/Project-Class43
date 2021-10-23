1b7a7035b04eb1412f5b6f4bb5384b5b421a7dfd
PrithvirajB06 <77888138+PrithvirajB06@users.noreply.github.com>
2021-10-23 13:01:32 +0530

    Add files via upload

---
+++ JumbledNames.js
@@ -0,0 +1,25 @@
+const accCode1 = "VARIABLE";
+const accCode2 = "FUNCTION";
+const accCode3 = "ALGORITHM";
+
+function clue() {
+    
+    fill("white")
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+
+    fill("white")
+    textSize(15);
+    text("C U T N I F O N", 700,150);
+    fill("lightblue");
+    text("Hint: Performs a particular task !!", 700,170);
+
+    fill("white")
+    textSize(15);
+    text("A O R I G T M L H", 100,250);
+    fill("lightblue");
+    text("Hint: Instructions to solve a problem !!", 100,270);
+
+}
